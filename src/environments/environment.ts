589152bc800543a0c8af7a42af0106091db4a1bd
Ilkka Ihalainen <ilkka.ihalainen@tuni.fi>
2023-08-17 12:15:44 +0300

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyB__6c1Hy9LwQq-iWKvXDqvLIzhsthfGT0",
+        authDomain: "oshop-ec4f0.firebaseapp.com",
+        projectId: "oshop-ec4f0",
+        storageBucket: "oshop-ec4f0.appspot.com",
+        messagingSenderId: "1034777579893",
+        appId: "1:1034777579893:web:24d4420d428d4d3aacc33c",
+        measurementId: "G-3KTX79P5DF"
+    }
+
+};
